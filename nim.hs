8c5b0315bb49d37b0a8a67c038267df0ca47cdfb
ron <rinbar70@gmail.com>
2022-09-02 16:54:14 +0300

    Elaborate.

--- nim.hs
+++ nim.hs
@@ -1,35 +1,50 @@
-module Main where
+module Main
+  ( main
+  ) where
+
+import           Text.Printf (printf)
 
 type State = [Int]
+
 type Move = (Int, Int)
 
 putState :: State -> IO ()
-putState = mapM_ print
+putState = mapM_ putStrLn . zipWith fmt [1 ..]
+ where
+  fmt :: Int -> Int -> String
+  fmt k n = printf "%d : %s" k (replicate n '*')
 
 getMove :: IO Move
--- TODO: Implement `getMove`
 getMove = do
   putStr "Row? "
   s <- getLine
-  let r = read s :: Int
+  let k = read s :: Int
   putStr "How many? "
   s <- getLine
   let m = read s :: Int
-  return (r, m)
+  return (k - 1, m)
+
+validMove :: Move -> State -> Bool
+validMove (k, m) s = 0 <= k && k < length s && m <= s !! k
 
 applyMove :: Move -> State -> State
--- TODO: Implement `applyMove`
-applyMove (k, m) s = [ if i == k then n - m else n | (i, n) <- zip [1 ..] s ]
+applyMove (k, m) s = take k s ++ [s !! k - m] ++ drop (k + 1) s
 
 finished :: State -> Bool
 finished = all (== 0)
 
 play :: State -> IO ()
-play s = do
-  putState s
-  (r, m) <- getMove
-  let s' = applyMove (r, m) s in if finished s' then return () else play s'
+play s = do putState s
+            (k, m) <- getMove
+            if validMove (k, m) s then do
+              let s' = applyMove (k, m) s
+              if finished s' then do
+                putState s'
+                putStrLn "** Game over. **\BEL"
+              else play s'
+            else do
+              putStrLn "** Invalid move. **\BEL"
+              play s
 
 main :: IO ()
 main = play [5, 4 .. 1]
-
